191a44a28197d7cfad47708818e657dc0b4b8054
Allen Do <ad@Allens-MacBook-Pro.local>
2020-02-18 03:40:10 -0800

    MVP

---
+++ to-do-reducers/src/components/todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Todo.css";
+const Todo = props => {
+    return (
+        <div
+            onClick={() => props.toggleCompleted(props.todo.id)}
+            className={`todo${props.todo.completed ? ' completed' : ''}`}
+            >
+                <p>{props.todo.item}</p>
+        </div>
+    );
+};
+
+
+
+export default Todo;
